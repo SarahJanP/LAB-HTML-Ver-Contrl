4edbfdfd44cbbaa38fc47d845e337a15a5538a0f
Sarah Parker <sarah.parker@edu.sait.ca>
2024-11-14 21:20:38 -0700

    Docs: Added Todo list for required javascript elements

--- javascript.js
+++ javascript.js
@@ -28,3 +28,24 @@ if (currentHours >= 17) {
 
 console.log(today.toDateString());
 console.log("Javascript File Loaded");         
+
+/* TODO:
+ALERT BUTTON: create button with the ID: "btn-alert"
+-> When button is cllicked use the alert() function to display a message
+
+Hover Button: Use Javaascrtipt to change the appeaaranace of thhe alert button so the text of the button c hanges  when
+the mouse hovers over it, anad then reverts when it leaves. May use DOM APII method:
+- getElementByID()
+- onmouseover
+- innerHTMLL or innerText
+
+For loops
+- Create a new ordered list with the id "numbers" in your HTML.
+- Using JS, create a for loop that populates the ordered list with 12 child elements of <li>List item</li>.
+- Now set the <li> text with either even or odd, to appropriately match the list item's index number.
+- Bonus: find a more efficient syntax for incrementing the counter variable used in the for loop.
+
+
+
+
+*/
